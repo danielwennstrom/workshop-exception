810cd1fd04aad0c19cd878b448afa6706d9c1770
Daniel Wennström <daniel@wennstrom.io>
2025-03-05 10:21:08 +0100

    Exception handling for CSVReader_Writer.getMaleFirstNames, CSVReader_Writer.getFemaleFirstNames

--- src/main/java/se/lexicon/exceptions/workshop/fileIO/CSVReader_Writer.java
+++ src/main/java/se/lexicon/exceptions/workshop/fileIO/CSVReader_Writer.java
@@ -2,6 +2,7 @@ package se.lexicon.exceptions.workshop.fileIO;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -11,66 +12,91 @@ import java.util.stream.Stream;
 
 public class CSVReader_Writer {
     //TODO:
+
     /**
      * This method getMaleFirstNames should use a try-catch-finally without resources
      * Should catch FileNotFoundException and IOException
      * You should also close the Buffered reader in the finally block
+     *
      * @return List<String>of male firstnames
      */
-    public static List<String> getMaleFirstNames(){
+    public static List<String> getMaleFirstNames() {
 
         BufferedReader reader = null;
-        List <String> names = null;
+        List<String> names = null;
 
 
-        	reader = Files.newBufferedReader(Paths.get("firstname_males.txt"));
+        try {
+            reader = Files.newBufferedReader(Paths.get("firstname_males.txt"));
             names = reader.lines()
                     .flatMap(line -> Stream.of(line.split(",")))
                     .collect(Collectors.toList());
 
-         	return names;
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found : " + e.getLocalizedMessage());
+        } catch (IOException e) {
+            System.err.println("IO error : " + e.getLocalizedMessage());
         }
-
-
-    //TODO:
-    /**
-     * This method getFemaleFirstNames should make use of a try-catch with resources
-     * @return
-     */
-    public static List<String> getFemaleFirstNames(){
-
-        List<String> names=null;
-
-            BufferedReader reader = Files.newBufferedReader(Paths.get("firstname_female.txt"))
-                names = reader.lines()
-                        .flatMap(line -> Stream.of(line.split(",")))
-                        .collect(Collectors.toList());
+        finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    System.err.println("IO error : " + e.getLocalizedMessage());
+                }
+            }
+        }
 
         return names;
     }
 
     //TODO:
+
+    /**
+     * This method getFemaleFirstNames should make use of a try-catch with resources
+     *
+     * @return
+     */
+    public static List<String> getFemaleFirstNames() {
+
+        List<String> names = null;
+
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get("firstname_female.txt"))) {
+            names = reader.lines()
+                    .flatMap(line -> Stream.of(line.split(",")))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            System.err.println("IO error: " + e.getLocalizedMessage());
+        }
+
+        return names;
+    }
+
+    //TODO:
+
     /**
      * This method fetches strings from a file and put them into a list
      * This method might throw IOException which due to the throws clause need to
      * be handled by the caller.
+     *
      * @return List <String> of last names
      * @throws IOException
      */
-    public static List<String> getLastNames() throws IOException{
+    public static List<String> getLastNames() throws IOException {
 
         List<String> names = null;
         BufferedReader reader = null;
 
-        try{
-                reader = Files.newBufferedReader(Paths.get("lastnames.txt"));
-                names = reader.lines()
-                .flatMap(line -> Stream.of(line.split(",")))
-                .collect(Collectors.toList());
+        try {
+            reader = Files.newBufferedReader(Paths.get("lastnames.txt"));
+            names = reader.lines()
+                    .flatMap(line -> Stream.of(line.split(",")))
+                    .collect(Collectors.toList());
 
 
-        }finally{
-            if(reader != null){
+        } finally {
+            if (reader != null) {
                 reader.close();
             }
         }
@@ -78,34 +104,33 @@ public class CSVReader_Writer {
     }
 
     //TODO:
-    public static void saveLastNames(List <String> lastNames){
+    public static void saveLastNames(List<String> lastNames) {
 
-            BufferedWriter writer = Files.newBufferedWriter(Paths.get("lastnames.txt"));
-            for(String toWrite : lastNames){
-                writer.append(toWrite+",");
-            }
-            writer.flush();
-      }
+        BufferedWriter writer = Files.newBufferedWriter(Paths.get("lastnames.txt"));
+        for (String toWrite : lastNames) {
+            writer.append(toWrite + ",");
+        }
+        writer.flush();
+    }
 
     //TODO:
-    public static void saveFemaleNames(List <String> femaleNames){
+    public static void saveFemaleNames(List<String> femaleNames) {
         BufferedWriter writer = Files.newBufferedWriter(Paths.get("firstname_female.txt"));
-            for(String toWrite : femaleNames){
-                writer.append(toWrite+",");
-            }
-            writer.flush();
+        for (String toWrite : femaleNames) {
+            writer.append(toWrite + ",");
+        }
+        writer.flush();
 
     }
 
 
-
     //TODO:
-    public static void saveMaleNames(List <String> maleNames){
+    public static void saveMaleNames(List<String> maleNames) {
         BufferedWriter writer = Files.newBufferedWriter(Paths.get("firstname_males.txt"));
-            for(String toWrite : maleNames){
-                writer.append(toWrite+",");
-            }
-            writer.flush();
+        for (String toWrite : maleNames) {
+            writer.append(toWrite + ",");
+        }
+        writer.flush();
 
 
     }
